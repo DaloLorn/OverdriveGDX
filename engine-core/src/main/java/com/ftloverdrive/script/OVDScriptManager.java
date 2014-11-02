d775b273e15a940597793d0c352e81126943fe69
kartoFlane <tbachminski@gmail.com>
2014-11-02 14:28:47 +0100

    Fleshed out OVDScriptManager a little

--- engine-core/src/main/java/com/ftloverdrive/script/OVDScriptManager.java
+++ engine-core/src/main/java/com/ftloverdrive/script/OVDScriptManager.java
@@ -1,18 +1,19 @@
 package com.ftloverdrive.script;
 
-import java.io.InputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
-import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.utils.Logger;
-
+import bsh.BshMethod;
+import bsh.CallStack;
 import bsh.EvalError;
 import bsh.Interpreter;
 import bsh.NameSpace;
 import bsh.Primitive;
 import bsh.UtilEvalError;
 
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Logger;
 import com.ftloverdrive.util.TextUtilities;
 
 
@@ -34,6 +35,44 @@ public class OVDScriptManager {
 	}
 
 	/**
+	 * Returns a new NameSpace object that can be used to hold variables and methods loaded
+	 * by scripts that use the same namespace. Global namespace is still visible.
+	 * 
+	 * Remember to call namespace.clear() when done.
+	 * 
+	 * @param name identifier of the namespace
+	 */
+	public NameSpace requestNewNameSpace( String name ) {
+		return new NameSpace( bsh.getNameSpace(), name );
+	}
+
+	/**
+	 * Returns a new NameSpace object that can be used to hold variables and methods loaded
+	 * by scripts that use the same namespace. Global namespace is still visible.
+	 * 
+	 * Remember to call namespace.clear() when done.
+	 * 
+	 * @param name identifier of the namespace
+	 * @param map
+	 *            A map of identifiers to objects -- the script will be able to access the objects
+	 *            by these identifiers. Basically, a way to setup variables available in the namespace.
+	 */
+	public NameSpace requestNewNameSpace( String name, Map<String, Object> vars ) {
+		NameSpace ns = new NameSpace( bsh.getNameSpace(), name );
+
+		try {
+			for ( Map.Entry<String, Object> entry : vars.entrySet() ) {
+				ns.setVariable( entry.getKey(),
+						Primitive.wrap( entry.getValue(), entry.getValue().getClass() ), false );
+			}
+		} catch ( UtilEvalError e ) {
+			log.error( "Error while evaluating variables.", e );
+		}
+
+		return ns;
+	}
+
+	/**
 	 * Evaluates a script file in the global namespace.
 	 *
 	 * @see TextUtilities.decodeText(InputStream srcStream, String srcDescription)
@@ -51,22 +90,11 @@ public class OVDScriptManager {
 	}
 
 	/**
-	 * Evaluates a script file in the specified namespace (global namespace is still accessible)
+	 * Evaluates a script file in the specified namespace (global namespace is still visible).
 	 * 
 	 * @see TextUtilities.decodeText(InputStream srcStream, String srcDescription)
 	 */
-	public void eval( FileHandle f, Map<String, Object> map ) throws EvalError, IOException {
-		NameSpace ns = new NameSpace(bsh.getNameSpace(), "nsTemp");
-
-		try {
-			for ( Map.Entry<String, Object> entry : map.entrySet() ) {
-				ns.setVariable( entry.getKey(),
-						Primitive.wrap( entry.getValue(), entry.getValue().getClass() ), false );
-			}
-		} catch ( UtilEvalError e ) {
-			log.error( "Error while evaluating variables.", e );
-		}
-
+	public void eval( FileHandle f, NameSpace ns ) throws EvalError, IOException {
 		InputStream is = null;
 		try {
 			is = f.read();
@@ -76,5 +104,58 @@ public class OVDScriptManager {
 			try {if ( is != null ) is.close();}
 			catch ( IOException e ) {}
 		}
+	}
+
+	/**
+	 * Evaluates the statements in the global namespace.
+	 */
+	public Object eval( String script ) throws EvalError {
+		return bsh.eval( script );
+	}
+
+	/**
+	 * Evaluates the statements in the specified namespace.
+	 */
+	public Object eval( String script, NameSpace ns ) throws EvalError {
+		return bsh.eval( script, ns );
+	}
+
+	/**
+	 * Invokes the specified method with the specified arguments and returns its value.
+	 * 
+	 * This method wraps arguments with Beanshell's wrappers as needed.
+	 * 
+	 * @param ns namespace containing the method
+	 * @param methodName name of the invoked method
+	 * @param args arguments to pass to the method
+	 * @return what the invoked method returned
+	 * 
+	 * @throws NoSuchMethodException when the requested method could not be found
+	 */
+	public Object invoke( NameSpace ns, String methodName, Object... args )
+			throws EvalError, NoSuchMethodException {
+		// Wrap args in Beanshell's wrappers
+		for ( int i = 0; i < args.length; i++ )
+			args[i] = Primitive.wrap( args[i], args[i].getClass() );
+
+		// Construct param signature
+		Class[] sig = new Class[args.length];
+		for ( int i = 0; i < args.length; i++ )
+			sig[i] = args[i].getClass();
+
+		BshMethod method = null;
+		try {
+			method = ns.getMethod(methodName, sig, true);
+		}
+		catch ( UtilEvalError e ) {
+			// UtilEvalErrors are EvalErrors that have no interpreter context
+			// They need to be re-thrown and supplied the current context
+			throw e.toEvalError( null, new CallStack( ns ) );
+		}
+
+		if ( method == null )
+			throw new NoSuchMethodException();
+
+		return method.invoke( args, bsh );
 	}
 }
