e5b6aa6f7f2664fd74d1716226614e2bd6e8eae9
unknown <tbachminski@gmail.com>
2015-05-19 02:17:38 +0200

    Updated SystemActor + missing files
    Too lazy to splice up the SystemActor file into separate commits.

--- engine-core/src/main/java/com/ftloverdrive/ui/hud/SystemActor.java
+++ engine-core/src/main/java/com/ftloverdrive/ui/hud/SystemActor.java
@@ -8,10 +8,11 @@ import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Disposable;
@@ -24,11 +25,15 @@ import com.ftloverdrive.event.system.SystemPropertyListener;
 import com.ftloverdrive.io.OVDSkin;
 import com.ftloverdrive.model.system.SystemModel;
 import com.ftloverdrive.ui.ModelActor;
+import com.ftloverdrive.ui.OVDTooltip;
+import com.ftloverdrive.ui.OVDTooltipListener;
 import com.ftloverdrive.util.OVDConstants;
 
 
 /**
  * Actor representing a system in the player's HUD.
+ * 
+ * TODO: This will need to be replaced by an interface + separate implementations for PC and Android.
  * 
  * @author kartoFlane
  */
@@ -37,7 +42,6 @@ public class SystemActor extends ModelActor
 
 	public static final String SKIN_PATH = "overdrive-assets/skins/player-hud/reactor-ui/system-ui.json";
 	public static final String ATLAS_PATH = "overdrive-assets/skins/player-hud/reactor-ui/reactor-ui.atlas";
-	public static final String LOCK_ATLAS_PATH = "img/icons/locking/.atlas.atlas";
 
 	protected final NinePatch barEmpty;
 	protected final NinePatch barFull;
@@ -47,6 +51,7 @@ public class SystemActor extends ModelActor
 
 	protected final Sprite lock;
 	protected final Sprite lockWhite;
+	protected final Sprite glow;
 
 	protected final Color colorHasPower;
 	protected final Color colorNoPower;
@@ -56,7 +61,11 @@ public class SystemActor extends ModelActor
 	protected final Color colorDestroyed;
 
 	protected final Image icon;
+	protected final Image iconHover;
+	protected final OVDTooltip tooltip;
+	protected final Actor tooltipTrigger;
 	protected final IonLockActor ionLock;
+	protected final SystemButtonsActor systemButtons;
 
 	protected final int sysBarWidth;
 	protected final int sysBarHeight;
@@ -68,6 +77,8 @@ public class SystemActor extends ModelActor
 	protected final int iconOffsetY;
 	protected final int barPaddingY;
 	protected final int barGroupPaddingY;
+	protected final int buttonOffsetX;
+	protected final int buttonOffsetY;
 
 	private final AssetManager assetManager;
 
@@ -93,7 +104,7 @@ public class SystemActor extends ModelActor
 		assetManager.finishLoading();
 
 		OVDSkin skin = assetManager.get( SKIN_PATH, OVDSkin.class );
-		TextureAtlas wireAtlas = assetManager.get( ATLAS_PATH, TextureAtlas.class );
+		TextureAtlas reactorAtlas = assetManager.get( ATLAS_PATH, TextureAtlas.class );
 		TextureAtlas lockAtlas = assetManager.get( OVDConstants.ION_LOCK_ATLAS, TextureAtlas.class );
 
 		colorHasPower = skin.getColor( "power-available" );
@@ -104,13 +115,13 @@ public class SystemActor extends ModelActor
 		colorDestroyed = skin.getColor( "power-destroyed" );
 
 		barDrawable = new NinePatchDrawable();
-		barEmpty = new NinePatch( wireAtlas.findRegion( "bar-empty" ), 1, 1, 1, 1 );
+		barEmpty = new NinePatch( reactorAtlas.findRegion( "bar-empty" ), 1, 1, 1, 1 );
 		barEmpty.setColor( colorNoPower );
-		barFull = new NinePatch( wireAtlas.findRegion( "bar-full" ), 1, 1, 1, 1 );
+		barFull = new NinePatch( reactorAtlas.findRegion( "bar-full" ), 1, 1, 1, 1 );
 		barFull.setColor( colorHasPower );
-		diagonal = new NinePatch( wireAtlas.findRegion( "diagonal" ) );
+		diagonal = new NinePatch( reactorAtlas.findRegion( "diagonal" ) );
 		diagonal.setColor( colorDisabled );
-		ionFrame = new NinePatch( wireAtlas.findRegion( "ion-frame" ), 2, 2, 2, 2 );
+		ionFrame = new NinePatch( reactorAtlas.findRegion( "ion-frame" ), 2, 2, 2, 2 );
 		ionFrame.setColor( colorIon );
 
 		sysBarWidth = skin.getInt( "system-bar-width" );
@@ -123,15 +134,35 @@ public class SystemActor extends ModelActor
 		ionFrameGapY = skin.getInt( "ion-frame-gap-y" ) + (int)ionFrame.getTopHeight();
 		barPaddingY = skin.getInt( "bar-padding-y" );
 		barGroupPaddingY = skin.getInt( "bar-group-padding-y" );
+		buttonOffsetX = skin.getInt( "button-base-offset-x" );
+		buttonOffsetY = skin.getInt( "button-base-offset-y" );
+
+		tooltipTrigger = new Actor();
+		addActor( tooltipTrigger );
 
 		icon = new Image();
+		icon.setScaling( Scaling.none );
 		addActor( icon );
+
+		iconHover = new Image();
+		iconHover.setScaling( Scaling.none );
+		iconHover.setTouchable( Touchable.disabled );
+		iconHover.setVisible( false );
+		addActor( iconHover );
+
+		tooltip = new OVDTooltip( context );
+		tooltip.addTo( tooltipTrigger );
 
 		lock = lockAtlas.createSprite( "s-lock" );
 		lockWhite = lockAtlas.createSprite( "s-lock-white" );
+		glow = reactorAtlas.createSprite( "glow" );
+		glow.setSize( sysBarWidth * 2, sysBarHeight * 2 );
 
 		ionLock = new IonLockActor( context );
 		addActor( ionLock );
+
+		systemButtons = new SystemButtonsActor( context );
+		addActor( systemButtons );
 
 		systemOffset = 0;
 		setSize( 0, 0 );
@@ -141,22 +172,23 @@ public class SystemActor extends ModelActor
 
 
 	public void drawChildren( Batch batch, float parentAlpha ) {
-		super.drawChildren( batch, parentAlpha );
-
-		// Draw power bars
 		float x = icon.getX() + ( clickableIconWidth - sysBarWidth ) * 0.5f;
 		float y = icon.getY() + clickableIconHeight + barOffsetY;
 
+		// Draw the ion frame around power bars
 		if ( powerIoned > 0 ) {
 			barDrawable.setPatch( ionFrame );
 			barDrawable.draw( batch, x - ionFrameGapX, y - ionFrameGapY,
 					2 * ionFrameGapX + sysBarWidth, 2 * ionFrameGapY + barsHeight );
 			barFull.setColor( colorIon );
+			glow.setColor( colorIon );
 		}
 		else {
 			barFull.setColor( colorHasPower );
+			glow.setColor( colorHasPower );
 		}
 
+		// Draw power bars
 		barDrawable.setPatch( barFull );
 		for ( int i = 0; i < powerCap; ++i ) {
 			if ( i >= powerCap - powerDestroyed ) {
@@ -166,6 +198,7 @@ public class SystemActor extends ModelActor
 
 				barDrawable.setPatch( barEmpty );
 				barEmpty.setColor( colorDestroyed );
+				glow.setColor( colorDestroyed );
 			}
 			else if ( i >= powerCap - ( powerDisabled + powerDestroyed ) ) {
 				barDrawable.setPatch( diagonal );
@@ -174,12 +207,21 @@ public class SystemActor extends ModelActor
 
 				barDrawable.setPatch( barEmpty );
 				barEmpty.setColor( colorDisabled );
+				glow.setColor( colorDisabled );
 			}
 			else if ( i >= powerCurrent && barDrawable.getPatch() != barEmpty ) {
 				barDrawable.setPatch( barEmpty );
 				barEmpty.setColor( colorNoPower );
+				glow.setColor( colorNoPower );
 			}
+
+			if ( iconHover.isVisible() ) {
+				glow.setPosition( x - sysBarWidth * 0.5f, y - sysBarHeight * 0.5f );
+				glow.draw( batch, 0.66f * parentAlpha );
+			}
+
 			barDrawable.draw( batch, x, y, sysBarWidth, sysBarHeight );
+
 			y += sysBarHeight;
 			if ( powerIncrement > 1 && i % powerIncrement == powerIncrement - 1 )
 				y += barGroupPaddingY;
@@ -187,12 +229,15 @@ public class SystemActor extends ModelActor
 				y += barPaddingY;
 		}
 
+		// Draw the ion padlock
 		if ( powerIoned > 0 ) {
 			lockWhite.setPosition( ( icon.getWidth() - lockWhite.getWidth() ) * 0.5f, y );
 			lock.setPosition( ( icon.getWidth() - lock.getWidth() ) * 0.5f, y );
 			lockWhite.draw( batch, parentAlpha );
 			lock.draw( batch, parentAlpha );
 		}
+
+		super.drawChildren( batch, parentAlpha );
 	}
 
 	/**
@@ -206,6 +251,7 @@ public class SystemActor extends ModelActor
 	public void setModelRefId( int modelRefId ) {
 		super.setModelRefId( modelRefId );
 		ionLock.setModelRefId( modelRefId );
+		systemButtons.setModelRefId( modelRefId );
 	}
 
 	@Override
@@ -235,6 +281,8 @@ public class SystemActor extends ModelActor
 			ionLock.setVisible( powerIoned > 0 );
 			// icon.setVisible( powerIoned == 0 ); // TODO: Commented out so that the actor remains clickable for testing purposes. Uncomment
 
+			updateTooltip( systemModel );
+
 			barsHeight = 0;
 			for ( int i = 0; i < powerCap; ++i ) {
 				barsHeight += sysBarHeight;
@@ -246,32 +294,72 @@ public class SystemActor extends ModelActor
 				}
 			}
 
-			setSize( icon.getWidth(), icon.getHeight() + iconOffsetY + barsHeight + barOffsetY + ionFrameGapY );
+			systemButtons.setPosition( icon.getX() + icon.getWidth() + buttonOffsetX, icon.getY() + buttonOffsetY );
+			systemButtons.updateInfo( context );
+
+			float w = icon.getWidth() + systemButtons.getWidth();
+			float h = icon.getHeight() + iconOffsetY + barsHeight + barOffsetY + ionFrameGapY;
+			setSize( w, h );
+			tooltipTrigger.setPosition( icon.getX(), icon.getY() );
+			tooltipTrigger.setSize( w, h );
 		}
 	}
 
+	private void updateTooltip( final SystemModel systemModel ) {
+		StringBuilder buf = new StringBuilder();
+
+		buf.append( systemModel.getTooltipSystemDescription() );
+		buf.append( "\n\n" );
+		String t = systemModel.getTooltipPowerDescription();
+		if ( t != null && !t.equals( "" ) ) {
+			buf.append( t );
+			buf.append( "\n\n" );
+		}
+		if ( systemModel.isManned() ) {
+			buf.append( "Manned: " ).append( systemModel.getTooltipManningDescription() );
+			buf.append( "\n\n" );
+		}
+		buf.append( "Status:\n" );
+		buf.append( systemModel.getTooltipStatusDescription() );
+
+		if ( !systemModel.isSelfPowered() ) {
+			buf.append( "\n\n" );
+			buf.append( "Add Power: " ).append( systemModel.getAddPowerKey() );
+			buf.append( "\n" );
+			buf.append( "Remove Power: " ).append( systemModel.getRemovePowerKey() );
+		}
+
+		tooltip.setText( buf.toString() );
+	}
+
 	private void updateIcon( final OverdriveContext context, final SystemModel model ) {
+		// TODO: Rewrite using GDX Button?
+
 		String textureName = model.getIconName();
 		if ( powerDestroyed == 0 )
 			if ( powerCurrent == 0 )
-				textureName += "-grey1";
+				textureName += "-grey";
 			else
-				textureName += "-green1";
+				textureName += "-green";
 		else if ( powerDestroyed < powerCap )
-			textureName += "-orange1";
+			textureName += "-orange";
 		else
-			textureName += "-red1";
+			textureName += "-red";
 
 		TextureAtlas iconAtlas = context.getAssetManager().get( OVDConstants.ICONS_ATLAS, TextureAtlas.class );
-		TextureRegion iconRegion = iconAtlas.findRegion( textureName );
+		TextureRegion iconRegion = iconAtlas.findRegion( textureName + "1" );
 		icon.setDrawable( new TextureRegionDrawable( iconRegion ) );
-		icon.setScaling( Scaling.none );
 		icon.setY( -( icon.getPrefHeight() - clickableIconHeight ) * 0.5f + iconOffsetY );
 		icon.setSize( clickableIconWidth, clickableIconHeight );
 
+		iconRegion = iconAtlas.findRegion( textureName + "2" );
+		iconHover.setDrawable( new TextureRegionDrawable( iconRegion ) );
+		iconHover.setY( -( icon.getPrefHeight() - clickableIconHeight ) * 0.5f + iconOffsetY );
+		iconHover.setSize( clickableIconWidth, clickableIconHeight );
+
 		if ( icon.getListeners().size > 0 )
 			icon.removeListener( icon.getListeners().get( 0 ) );
-		icon.addListener( new ClickListener() {
+		icon.addListener( new InputListener() {
 			@Override
 			public boolean touchDown( InputEvent event, float x, float y, int pointer, int button ) {
 				// TODO: For testing purposes only, remove.
@@ -291,12 +379,12 @@ public class SystemActor extends ModelActor
 				}
 
 				if ( model.isSelfPowered() ) {
-					// TODO: sounds + info
+					// TODO: sounds
 					return true;
 				}
 
 				if ( powerIoned > 0 ) {
-					// TODO: sounds + info
+					// TODO: sounds
 					return true;
 				}
 
@@ -315,6 +403,32 @@ public class SystemActor extends ModelActor
 					return true;
 				}
 				return false;
+			}
+
+			public boolean mouseMoved( InputEvent event, float x, float y ) {
+				return ( (InputListener)tooltipTrigger.getListeners().get( 0 ) ).mouseMoved( event, x, y );
+			}
+
+			public void enter( InputEvent event, float x, float y, int pointer, Actor fromActor ) {
+				if ( fromActor != icon )
+					iconHover.setVisible( true );
+
+				OVDTooltipListener l = (OVDTooltipListener)tooltipTrigger.getListeners().get( 0 );
+				float temp = l.getShowDelay();
+				if ( fromActor == tooltipTrigger )
+					l.setShowDelay( 0 );
+				l.enter( event, x, y, pointer, fromActor );
+				if ( fromActor == tooltipTrigger )
+					l.setShowDelay( temp );
+			}
+
+			public void exit( InputEvent event, float x, float y, int pointer, Actor toActor ) {
+				if ( toActor != icon )
+					iconHover.setVisible( false );
+
+				OVDTooltipListener l = (OVDTooltipListener)tooltipTrigger.getListeners().get( 0 );
+				if ( toActor != tooltipTrigger )
+					l.exit( event, x, y, pointer, toActor );
 			}
 		} );
 	}
